4338cd0c9ba1d820e1bba7461997c6c1baf7420b
SithuKyaw-Thitsaworks <sithu.kyaw@thitsaworks.com>
2023-06-07 14:17:22 +0630

    Remove http-interceptor from util

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrandCreateComponent } from './components/brands/brand-create/brand-create.component';
 import { AngularEditorModule } from '@kolkov/angular-editor';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { CustomHttpInterceptor } from './utils/http-interceptor';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { LoaderInterceptor } from './utils/loader-interceptor';
 import { LoaderComponent } from './components/shared/loader/loader.component';
