13cf313ed234e8d435876a509b0522b844f58efd
SithuKyaw-Thitsaworks <113161065+SithuKyaw-Thitsaworks@users.noreply.github.com>
2024-06-01 20:52:10 +0700

    fix: fix merge conflict

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { AuthService } from './services/auth.service';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) {}
-
-  canActivate(): Observable<boolean> {
-    return this.authService.isAuthenticated().pipe(
-      map(isAuthenticated => {
-        debugger
-        if (isAuthenticated) {
-          return true;
-        } else {
-          this.router.navigate(['/login']);
-          return false;
-        }
-      })
-    );
-  }
-}
-=======
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
@@ -51,4 +23,3 @@ export class AuthGuard implements CanActivate {
     );
   }
 }
->>>>>>> 29e195d518aa4b1b23533cc902d31e4ed2f15ab0
--- src/app/components/products/product-batch-create/product-batch-create.component.ts
+++ src/app/components/products/product-batch-create/product-batch-create.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
@@ -67,73 +66,3 @@ export class ProductBatchCreateComponent {
         this.dialogRef.close();
     }
 }
-=======
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
-import { IProductBatch } from 'src/app/models/productBatch';
-
-@Component({
-    selector: 'app-product-batch-create',
-    templateUrl: './product-batch-create.component.html',
-    styleUrls: ['./product-batch-create.component.css']
-})
-export class ProductBatchCreateComponent {
-    @Output() productBatchAdded: EventEmitter<IProductBatch> = new EventEmitter<IProductBatch>();
-    productBatchForm: FormGroup;
-
-    constructor(
-        private formBuilder: FormBuilder,
-        private dialogRef: MatDialogRef<ProductBatchCreateComponent>
-    ) {
-        this.productBatchForm = this.formBuilder.group({
-            createdDate: [''],
-            mnuDate: [''],
-            expDate: [''],
-            quantity: [''],
-            note: [''],
-            purchasePrice: [''],
-            sellingPrice: [''],
-            isPromotionitem: [''],
-            promotionPrice: ['']
-
-        });
-    }
-
-    onSubmit(): void {
-        if (this.productBatchForm.valid) {
-            const productBatch: IProductBatch = this.productBatchForm.value;
-            // Perform save or update operation using the brand data
-            this.addProductBatch();
-        }
-    }
-
-    addProductBatch() {
-        if (this.productBatchForm.invalid) {
-            return;
-        }
-
-        const addedProductBatch:IProductBatch = {
-            createdDate: 0,
-            mnuDate: new Date(this.productBatchForm.value.mnuDate).getTime() / 1000,
-            expDate: new Date(this.productBatchForm.value.expDate).getTime() /1000,
-            quantity: this.productBatchForm.value.quantity,
-            note: this.productBatchForm.value.note,
-            purchasePrice: this.productBatchForm.value.purchasePrice,
-            sellingPrice: this.productBatchForm.value.sellingPrice,
-            isPromotionItem: this.productBatchForm.value.isPromotionItem,
-            promotionPrice: this.productBatchForm.value.promotionPrice
-            
-        };
-
-        this.productBatchAdded.emit(addedProductBatch);
-        this.productBatchForm.reset();
-        this.dialogRef.close();
-        
-    }
-
-    cancel(): void {
-        this.dialogRef.close();
-    }
-}
->>>>>>> 29e195d518aa4b1b23533cc902d31e4ed2f15ab0
